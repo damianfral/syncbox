93a93b2b05395bfcfade0b342c4bbf6c821be419
damianfral <huevofritopamojarpan@gmail.com>
2023-01-03 05:02:37 +0000

    Restructure.

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "syncbox";
 
   inputs = {
-    nixpkgs = {url = "github:NixOS/nixpkgs/master";};
-    nixops-plugged = {url = "github:lukebfox/nixops-plugged";};
-    mne = {url = "github:damianfral/merge-nix-envs";};
-    flake-utils = {url = "github:numtide/flake-utils";};
+    nixpkgs = { url = "github:NixOS/nixpkgs/release-22.11"; };
+    nixops-plugged = { url = "github:lukebfox/nixops-plugged"; };
+    nix-filter.url = "github:numtide/nix-filter";
+    flake-utils = { url = "github:numtide/flake-utils"; };
     streamly = {
       url = "github:composewell/streamly";
       flake = false;
@@ -16,202 +16,229 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    mne,
-    flake-utils,
-    nixops-plugged,
-    ...
-  } @ inputs: let
-    pkgsFor = system:
-      import nixpkgs {
-        inherit system;
-        overlays = [];
-      };
-  in
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        pkgs = pkgsFor system;
-        mk-syncbox = returnShellEnv:
-          pkgs.haskellPackages.developPackage {
-            root = ./.;
-            source-overrides = {};
-            overrides = new: old: let
-              dontCheck = pkgs.haskell.lib.dontCheck;
-              unmarkBroken = pkgs.haskell.lib.unmarkBroken;
-            in {
-              streamly = old.callCabal2nix "streamly" inputs.streamly {};
-              streamly-core = old.callCabal2nix "streamly-core" "${inputs.streamly}/core" {};
-              streamly-process = dontCheck (old.callCabal2nix "streamly-process" inputs.streamly-process {});
-            };
-            modifier = drv:
-              if returnShellEnv
-              then
-                pkgs.haskell.lib.addBuildTools drv
-                (
-                  with pkgs.haskellPackages; [
-                    cabal-install
-                    ghcid
-                    haskell-language-server
-                    hlint
-                    ormolu
-                  ]
-                )
-              else pkgs.haskell.lib.justStaticExecutables drv;
-            inherit returnShellEnv;
-          };
-      in rec {
-        packages = rec {
-          syncbox = mk-syncbox false;
-          syncbox-shell = mk-syncbox true;
-
-          dev-shell = pkgs.mkShell {
-            buildInputs = with pkgs; [alejandra];
-            shellHook = ''
-              syncthing-proxy(){
-                nixops ssh -d fake.com fake -L 9999:localhost:8384 'echo "proxy to syncthing at http://localhost:9999"; read  -n 1'
-            '';
-          };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , nixops-plugged
+    , nix-filter
+    , ...
+    } @ inputs:
+    let
+      pkgsFor = system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.${system} nix-filter.overlays.default ];
         };
-        defaultPackage = packages.syncbox;
+    in
+    flake-utils.lib.eachDefaultSystem
+      (
+        system:
+        let
+          pkgs = pkgsFor system;
+          filteredSrc =
+            pkgs.nix-filter {
+              root = ./.;
+              include = [
+                "src/"
+                "test/"
+                "package.yaml"
+                "LICENSE"
+              ];
+            };
+        in
+        rec {
+          overlays = final: prev: with final.haskell.lib; {
+            haskellPackages = prev.haskellPackages.override (old: {
+              overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
+                (self: super:
+                  let
+                    dontCheck = pkgs.haskell.lib.dontCheck;
+                    unmarkBroken = pkgs.haskell.lib.unmarkBroken;
+                  in
 
-        devShell = with packages;
-          mne.lib.mergeNixEnvs
-          [syncbox-shell dev-shell];
+                  {
+                    streamly = self.callCabal2nix "streamly" inputs.streamly { };
+                    streamly-core = self.callCabal2nix "streamly-core" "${inputs.streamly}/core" { };
+                    streamly-process = dontCheck (self.callCabal2nix "streamly-process" inputs.streamly-process { });
+                    syncbox = self.generateOptparseApplicativeCompletions
+                      [ "syncbox" ]
+                      (self.callCabal2nix "syncbox" filteredSrc { });
+                  }
+                );
+            });
+          };
+          packages = {
+            syncbox =
+              pkgs.haskell.lib.justStaticExecutables (
+                pkgs.haskellPackages.syncbox.overrideAttrs
+                  (oldAttrs: {
+                    nativeBuildInputs = oldAttrs.nativeBuildInputs
+                    ++ [ pkgs.makeWrapper ];
+                    postInstall = (oldAttrs.postInstall or "") + ''
+                      wrapProgram $out/bin/syncbox \
+                        --suffix PATH : ${pkgs.lib.makeBinPath [pkgs.github-cli]}
+                    '';
+                  }
+                  ));
+          };
 
-        overlay = final: prev: { syncbox = packages.syncbox;};
-      }
-    )
-    // {
-      nixopsConfigurations.default = let
-        system = "x86_64-linux";
-        pkgs = pkgsFor system;
-        syncbox = self.packages.${system}.syncbox;
-        region = "eu-west-1";
-        accessKeyId = "fake";
-      in {
-        inherit nixpkgs;
-        network.description = "syncbox.com";
-        resources = {
-          ec2KeyPairs.syncbox-keys = {inherit region accessKeyId;};
-          ec2SecurityGroups.ssh-http-https = {
-            inherit region accessKeyId;
-            name = "ssh-http-https";
-            description = "syncbox security group";
-            rules = [
-              {
-                fromPort = 22;
-                toPort = 22;
-                sourceIp = "0.0.0.0/0";
-              }
-              {
-                fromPort = 80;
-                toPort = 80;
-                sourceIp = "0.0.0.0/0";
-              }
-              {
-                fromPort = 443;
-                toPort = 443;
-                sourceIp = "0.0.0.0/0";
-              }
+          defaultPackage = packages.syncbox;
+
+          devShells.default = pkgs.haskellPackages.shellFor {
+            packages = p: [ packages.syncbox ];
+            buildInputs = with pkgs; with pkgs.haskellPackages; [
+              haskell-language-server
+              cabal-install
+              ghcid
+              hpack
+              hlint
+              yamlfix
             ];
           };
-        };
-        syncbox = {
-          resources,
-          pkgs,
-          ...
-        }: {
-          deployment = {
-            targetEnv = "ec2";
-            ec2 = {
-              accessKeyId = accessKeyId;
-              region = region;
-              instanceType = "t3a.nano";
-              keyPair = resources.ec2KeyPairs.syncbox-keys;
-              # ebsInitialRootDiskSize = 20;
-              # ebsBoot = true;
-              associatePublicIpAddress = true;
-              securityGroups = [
-                resources.ec2SecurityGroups.ssh-http-https
+
+          # dev-shell = pkgs.mkShell {
+          #   buildInputs = with pkgs; [ alejandra ];
+          #   shellHook = ''
+          #     syncthing-proxy(){
+          #       nixops ssh -d fake.com fake -L 9999:localhost:8384 'echo "proxy to syncthing at http://localhost:9999"; read  -n 1'
+          #   '';
+          # };
+          # };
+
+
+        }
+      )
+    // {
+      nixopsConfigurations.default =
+        let
+          system = "x86_64-linux";
+          pkgs = pkgsFor system;
+          syncbox = self.packages.${system}.syncbox;
+          region = "eu-west-1";
+          accessKeyId = "fake";
+        in
+        {
+          inherit nixpkgs;
+          network.description = "syncbox.com";
+          resources = {
+            ec2KeyPairs.syncbox-keys = { inherit region accessKeyId; };
+            ec2SecurityGroups.ssh-http-https = {
+              inherit region accessKeyId;
+              name = "ssh-http-https";
+              description = "syncbox security group";
+              rules = [
+                {
+                  fromPort = 22;
+                  toPort = 22;
+                  sourceIp = "0.0.0.0/0";
+                }
+                {
+                  fromPort = 80;
+                  toPort = 80;
+                  sourceIp = "0.0.0.0/0";
+                }
+                {
+                  fromPort = 443;
+                  toPort = 443;
+                  sourceIp = "0.0.0.0/0";
+                }
               ];
             };
           };
-
-          networking.hostName = "syncbox";
-          networking.firewall.enable = false;
-          networking.firewall.allowedTCPPorts = [22 80 443 8000 8384 22000 22067 22070];
-          networking.firewall.allowedUDPPorts = [21027 22000];
-          networking.enableIPv6 = true;
-
-          environment.systemPackages = [pkgs.bash pkgs.fish];
-
-          nix.gc.automatic = true;
-
-          users.users.damian = {
-            isNormalUser = true;
-            createHome = true;
-            extraGroups = ["wheel"];
-            group = "users";
-          };
-
-          services.openssh.extraConfig = ''
-            MaxAuthTries 40
-          '';
-
-          services.cfdyndns = {
-            enable = true;
-            apikeyFile = ''${pkgs.writeText "cloudflare-apikey" "fake"}'';
-            email = "huevofritopamojarpan@gmail.com";
-            records = ["fake-url"];
-          };
-
-          services.syncthing = {
-            enable = true;
-            relay.enable = true;
-            openDefaultPorts = true;
-          };
-
-          services.traefik = {
-            enable = true;
-            staticConfigOptions = {
-              entryPoints.http.address = ":80";
-              pilot.token = "fake-token";
-            };
-            dynamicConfigOptions = {
-              http = {
-                services = {
-                  syncbox-server = {
-                    loadBalancer.servers = [{url = "http://localhost:3003";}];
-                  };
+          syncbox =
+            { resources
+            , pkgs
+            , ...
+            }: {
+              deployment = {
+                targetEnv = "ec2";
+                ec2 = {
+                  accessKeyId = accessKeyId;
+                  region = region;
+                  instanceType = "t3a.nano";
+                  keyPair = resources.ec2KeyPairs.syncbox-keys;
+                  # ebsInitialRootDiskSize = 20;
+                  # ebsBoot = true;
+                  associatePublicIpAddress = true;
+                  securityGroups = [
+                    resources.ec2SecurityGroups.ssh-http-https
+                  ];
                 };
-                syncbox-router = {
-                  router4 = {
-                    service = "syncbox";
-                    rule = "Host(`fake.com`)";
-                    entryPoints = ["http"];
+              };
+
+              networking.hostName = "syncbox";
+              networking.firewall.enable = false;
+              networking.firewall.allowedTCPPorts = [ 22 80 443 8000 8384 22000 22067 22070 ];
+              networking.firewall.allowedUDPPorts = [ 21027 22000 ];
+              networking.enableIPv6 = true;
+
+              environment.systemPackages = [ pkgs.bash pkgs.fish ];
+
+              nix.gc.automatic = true;
+
+              users.users.damian = {
+                isNormalUser = true;
+                createHome = true;
+                extraGroups = [ "wheel" ];
+                group = "users";
+              };
+
+              services.openssh.extraConfig = ''
+                MaxAuthTries 40
+              '';
+
+              services.cfdyndns = {
+                enable = true;
+                apikeyFile = ''${pkgs.writeText "cloudflare-apikey" "fake"}'';
+                email = "huevofritopamojarpan@gmail.com";
+                records = [ "fake-url" ];
+              };
+
+              services.syncthing = {
+                enable = true;
+                relay.enable = true;
+                openDefaultPorts = true;
+              };
+
+              services.traefik = {
+                enable = true;
+                staticConfigOptions = {
+                  entryPoints.http.address = ":80";
+                  pilot.token = "fake-token";
+                };
+                dynamicConfigOptions = {
+                  http = {
+                    services = {
+                      syncbox-server = {
+                        loadBalancer.servers = [{ url = "http://localhost:3003"; }];
+                      };
+                    };
+                    syncbox-router = {
+                      router4 = {
+                        service = "syncbox";
+                        rule = "Host(`fake.com`)";
+                        entryPoints = [ "http" ];
+                      };
+                    };
                   };
                 };
               };
-            };
-          };
 
-          systemd.services.syncbox = {
-            description = "syncbox";
-            wantedBy = ["multi-user.target"];
-            enable = true;
+              systemd.services.syncbox = {
+                description = "syncbox";
+                wantedBy = [ "multi-user.target" ];
+                enable = true;
 
-            after = ["network.target"];
-            serviceConfig = {
-              StandardOutput = "journal+console";
-              ExecStart = ''
-                ${syncbox}/bin/syncbox --port 3003 --root /var/lib/syncthing/Public
-              '';
+                after = [ "network.target" ];
+                serviceConfig = {
+                  StandardOutput = "journal+console";
+                  ExecStart = ''
+                    ${syncbox}/bin/syncbox --port 3003 --root /var/lib/syncthing/Public
+                  '';
+                };
+              };
             };
-          };
         };
-      };
     };
 }
